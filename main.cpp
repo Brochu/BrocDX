841503636811147c5c3c10b2910f737850796151
Brochu <alexbrochu1@gmail.com>
2024-09-29 15:06:23 -0400

    [FIX] Notify when sample application is closing

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 #include "brocdx.h"
 
 int main(int argc, char **argv) {
-    printf("[MAIN] Starting application\n");
+    printf("[APP] Starting application\n");
     bdx_open();
 
     bdx_close();
+    printf("[APP] Stopping application\n");
     return EXIT_SUCCESS;
 }
