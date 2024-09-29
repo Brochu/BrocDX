35aae596a46a61e7f7e9cff6990f12045cf41060
Brochu <alexbrochu1@gmail.com>
2024-09-28 23:50:19 -0400

    [SETUP] Link with DXGI for the core file, access to Dx12 factory creation functions

--- bdxcore.cpp
+++ bdxcore.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+#include "dxgi1_6.h"
+#include "DirectXMath.h"
+#include "wrl/client.h"
+
+using namespace Microsoft::WRL;
+
 void bdx_open() {
+    using namespace DirectX;
+
     printf("[LIB] Opening library\n");
+    XMMATRIX res = XMMatrixIdentity();
+    XMMATRIX transform = XMMatrixTranslation(1.f, 1.f, 1.f);
+    res = XMMatrixMultiply(res, transform);
+
+    ComPtr<IDXGIFactory7> factory;
+    HRESULT hr = CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
+    printf("[LIB] Create DXGI Factory result = %ld\n", hr);
+
 }
 
 void bdx_close() {
--- libbuild.bat
+++ libbuild.bat
@@ -1,4 +1,4 @@
 @echo off
 
 cl -nologo -c -std:c++14 bdxcore.cpp
-lib -nologo bdxcore.obj -out:brocdx.lib
+lib -nologo bdxcore.obj dxgi.lib -out:brocdx.lib
