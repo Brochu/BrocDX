7f3ce6a1c7a38644fe18c7993bac99b8dbbb9a34
Brochu <alexbrochu1@gmail.com>
2024-09-29 15:06:37 -0400

    [DEV] Starting work on Dx12 initialization (with debug layers)

--- bdxcore.cpp
+++ bdxcore.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 #include "dxgi1_6.h"
+#include "d3d12.h"
 #include "DirectXMath.h"
+
 #include "wrl/client.h"
 
 using namespace Microsoft::WRL;
@@ -9,17 +11,33 @@ using namespace Microsoft::WRL;
 void bdx_open() {
     using namespace DirectX;
 
-    printf("[LIB] Opening library\n");
+    printf("[BDX] Opening library\n");
     XMMATRIX res = XMMatrixIdentity();
     XMMATRIX transform = XMMatrixTranslation(1.f, 1.f, 1.f);
     res = XMMatrixMultiply(res, transform);
 
+    HRESULT hr;
     ComPtr<IDXGIFactory7> factory;
-    HRESULT hr = CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
-    printf("[LIB] Create DXGI Factory result = %ld\n", hr);
+    hr = CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&factory));
+    printf("[BDX] Create DXGI Factory result = %ld\n", hr);
 
+    ComPtr<IDXGIAdapter1> adapter;
+    hr = factory->EnumAdapters1(0, &adapter);
+    printf("[BDX] Getting first adapter = %ld\n", hr);
+
+    DXGI_ADAPTER_DESC1 desc;
+    hr = adapter->GetDesc1(&desc);
+    printf("[BDX] Getting description = (%ld) %ls\n", hr, desc.Description);
+
+    ComPtr<ID3D12Debug> debug;
+    hr = D3D12GetDebugInterface(IID_PPV_ARGS(&debug));
+    printf("[BDX] Getting debug interface = (%ld)\n", hr);
+    if (debug.Get()) {
+        printf("[BDX] Starting debug layers\n");
+        debug->EnableDebugLayer();
+    }
 }
 
 void bdx_close() {
-    printf("[LIB] Closing library\n");
+    printf("[BDX] Closing library\n");
 }
