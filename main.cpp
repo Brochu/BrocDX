6492e2a85c57e1c81f5d7c2ea3d9c8c5ae2fe313
Brochu <alexbrochu1@gmail.com>
2024-09-30 00:12:19 -0400

    [DEV] Switch to windows subsystem for sample app, alloc console manually for output

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
 #include "brocdx.h"
 
-int main(int argc, char **argv) {
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    if (!AllocConsole()) {
+        return 1;
+    }
+    FILE *empty;
+    freopen_s(&empty, "CONIN$", "r", stdin);
+    freopen_s(&empty, "CONOUT$", "w", stdout);
+    freopen_s(&empty, "CONOUT$", "w", stderr);
+
     printf("[APP] Starting application\n");
     bdx_start();
 
@@ -10,5 +20,7 @@ int main(int argc, char **argv) {
 
     bdx_stop();
     printf("[APP] Stopping application\n");
+
+    FreeConsole();
     return 0;
 }
